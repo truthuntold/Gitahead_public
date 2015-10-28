60cbd0c46a5ff769d2e8f316a6c7eadfc4538a45
Bryan Williams <truthuntold@gmail.com>
2015-10-28 09:36:04 -0600

    add test file

---
+++ Badge.cpp
@@ -0,0 +1,30 @@
+#include "Badge.h"
+#include <QPainter>
+
+Badge::Badge(const QString &text, QWidget *parent)
+  : QWidget(parent), mText(text)
+{
+  setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+}
+
+QSize Badge::sizeHint() const
+{
+  QString text = (mText.length() > 1) ? mText : "M";
+  QRect bounds = fontMetrics().boundingRect(text);
+  return QSize(bounds.width() + 12, bounds.height() + 2);
+}
+
+void Badge::paintEvent(QPaintEvent *event)
+{
+  QRect rect(QPoint(0, 0), sizeHint());
+
+  QPainter painter(this);
+  painter.setRenderHints(QPainter::Antialiasing);
+
+  painter.setPen(Qt::NoPen);
+  painter.setBrush(QColor("#A6ACB6"));
+  painter.drawRoundedRect(rect, 4, 4);
+
+  painter.setPen(Qt::white);
+  painter.drawText(rect, Qt::AlignHCenter | Qt::AlignVCenter, mText);
+}
