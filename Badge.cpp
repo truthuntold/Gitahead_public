da7c959b7f7ddb95b0767f79bfa43fdd949e1a30
Bryan Williams <truthuntold@gmail.com>
2015-10-28 09:42:19 -0600

    conflict

--- Badge.cpp
+++ Badge.cpp
@@ -7,7 +7,7 @@ Badge::Badge(const QString &text, QWidget *parent)
   setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 }
 
-QSize Badge::sizeHintNotTest() const
+QSize Badge::sizeHintTaco1111() const
 {
   QString text = (mText.length() > 1) ? mText : "M";
   QRect bounds = fontMetrics().boundingRect(text);
