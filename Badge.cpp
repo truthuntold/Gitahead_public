61ea0356742d9c3750a2e98fc64e1e2ab5bd218d
Bryan Williams <truthuntold@gmail.com>
2015-11-02 15:13:50 -0700

    live

--- Badge.cpp
+++ Badge.cpp
@@ -7,7 +7,7 @@ Badge::Badge(const QString &text, QWidget *parent)
   setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 }
 
-QSize Badge::sizeHintG4() const
+QSize Badge::sizeHintLive() const
 {
   QString text = (mText.length() > 1) ? mText : "M";
   QRect bounds = fontMetrics().boundingRect(text);
