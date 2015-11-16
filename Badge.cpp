da9d783e295067f00cc49011ddf5a3765d66e25a
Bryan Williams <truthuntold@gmail.com>
2015-11-16 10:17:59 -0700

    gg

--- Badge.cpp
+++ Badge.cpp
@@ -12,7 +12,7 @@ QSize Badge::sizeHintLive() const
   QString texttt = (mText.length() > 1) ? mText : "M";
   QRect bounds = fontMetrics().boundingRect(text);
   QString text = (mText.length() > 1) ? mText : "M";
-  QRect boundsChangedgg = fontMetrics().boundingRect(text);
+  QRect boundsChanged = fontMetrics().boundingRect(text);
   return QSize(bounds.width() + 12, bounds.height() + 2);
 }
 
